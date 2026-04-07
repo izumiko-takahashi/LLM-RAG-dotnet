a3bdc7185ef129fc239e901a311c39de0bd84016
sayamisuzu <sayamisuzu@comunidad.unam.mx>
2026-04-07 01:22:57 -0600

    feat: Registrando las dependencias a la Infraestructura en el contenedor de Inyección de dependencias

---
+++ src/LlmRag.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using LlmRag.Application.Interfaces;
+using LlmRag.Infrastructure.LLM;
+using LlmRag.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LlmRag.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IDocumentSearch, DocumentRepository>();
+        services.AddScoped<IResponseGenerator, LlmConection>();
+        return services;
+    }
+}
