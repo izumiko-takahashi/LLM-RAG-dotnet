db26433084c22dcfdd520b0010d83264374deca9
sayamisuzu <sayamisuzu@comunidad.unam.mx>
2026-04-06 23:28:27 -0600

    feat: Agregamos el orquestador del sistema RAG definiendo el servicio AskAsync

---
+++ src/LlmRag.Application/Services/RagService.cs
@@ -0,0 +1,22 @@
+﻿using LlmRag.Application.Interfaces;
+
+namespace LlmRag.Application.Services;
+
+public class RagService
+{
+    private readonly IDocumentSearch _documentSearch;    //Mejor recibimos desde fuera los métodos, aplicando la inyección de dependencias.
+    private readonly IResponseGenerator _responseGenerator;  //Podrían quedar dentro del constructor como new, pero perdería flexibilidad, casando el servicio a lo definido en el constructor.
+
+    public RagService(IDocumentSearch documentSearch, IResponseGenerator responseGenerator)
+    {
+        _documentSearch = documentSearch;
+        _responseGenerator = responseGenerator;
+    }
+
+    public async Task<string> AskAsync(string question, string fileName)
+    {
+        var extract = await _documentSearch.SearchAsync(question, fileName);
+        var response = await _responseGenerator.GenerateAsync(extract);
+        return response;
+    }
+}
